c1889f1c6fe6b6f485812b6974a054c485f11f87
SimoneS86 <simone.sensini.86@gmail.com>
2023-03-16 20:41:20 +0100

    step2

--- script1.js
+++ script1.js
@@ -1,9 +1,10 @@
 const loadImagesBtn = document.getElementById("load-images-btn");
-const arrayCardSvg = document.querySelectorAll(".changePic");
+const loadSecondImagesBtn = document.getElementById("load-second-images-btn");
+const arrayCard = document.querySelectorAll(".card");
 
 loadImagesBtn.addEventListener("click", async () => {
   try {
-    const query = "natura";
+    const query = "tigri";
     const url = `https://api.pexels.com/v1/search?query=${query}`;
     const response = await fetch(url, {
       headers: {
@@ -19,13 +20,45 @@ loadImagesBtn.addEventListener("click", async () => {
     const photos = data.photos;
 
     photos.forEach((photo, index) => {
-      if (index < arrayCardSvg.length) {
+      if (index < arrayCard.length) {
         const img = document.createElement("img");
-        img.src = photo.src.large2x;
+        img.src = photo.src.medium;
         img.alt = photo.photographer;
         img.width = 300;
         img.height = 200;
-        arrayCardSvg[index].parentNode.replaceChild(img, arrayCardSvg[index]);
+        arrayCard[index].replaceChild(img, arrayCard[index].firstElementChild);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+loadSecondImagesBtn.addEventListener("click", async () => {
+  try {
+    const query2 = "natura";
+    const url = `https://api.pexels.com/v1/search?query=${query2}`;
+    const response = await fetch(url, {
+      headers: {
+        Authorization: "a2zxBaLOgwriXftJaDVT8KEyFDhu1C7rgsQ4R4ZTXoedL5ZrJyUf0Csb",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    const photos = data.photos;
+
+    photos.forEach((photo, index) => {
+      if (index < arrayCard.length) {
+        const img = document.createElement("img");
+        img.src = photo.src.medium;
+        img.alt = photo.photographer;
+        img.width = 300;
+        img.height = 200;
+        arrayCard[index].replaceChild(img, arrayCard[index].firstElementChild);
       }
     });
   } catch (error) {
